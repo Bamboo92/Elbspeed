404f462aafefe746e51a4648bfd8ab1de10d3da5
Bamboo <75183449+Bamboo92@users.noreply.github.com>
2026-04-19 12:18:25 -0700

    Newest Update

---
+++ data/cars.js
@@ -0,0 +1,294 @@
+export const cars = [
+    {
+        "id": "BMW M4 Competition",
+        "name": "M4 Competition",
+        "thumbnail": "/images/bmw_m4_competition_front_left_corner.jpg",
+        "images": [
+            "/images/bmw_m4_competition_front_left_corner.jpg",
+            "/images/bmw_m4_competition_front.jpg",
+            "/images/bmw_m4_competition_m4_logo_rear.jpg",
+            "/images/bmw_m4_competition_left_side.jpg",
+            "/images/bmw_m4_competition_rear.jpg",
+            "/images/bmw_m4_competition_m4_logo_side.jpg",
+            "/images/bmw_m4_competition_rear_right_corner.jpg"
+        ],
+
+        "specs": {
+            "power_hp": "510 PS",
+            "torque_nm": "650 NM",
+            "vmax_kmh": "290 km/h",
+            "acceleration_0_100": "3,9 s"
+        },
+
+        "prices": [
+            {
+                "id": "day-week",
+                "name": "Tagesmiete",
+                "days": "Montag - Donnerstag",
+                "duration_h": "24",
+                "currency": "€",
+                "price": "300",
+                "included_km": "250",
+                "added_km": "1,50",
+                "penalty_km": "2,00",
+                "plan": "basic"
+            },
+            {
+                "id": "day-weekend",
+                "name": "Tagesmiete",
+                "days": "Freitag - Sonntag",
+                "duration_h": "24",
+                "currency": "€",
+                "price": "350",
+                "included_km": "250",
+                "added_km": "1,50",
+                "penalty_km": "2,00",
+                "plan": "basic"
+            },
+            {
+                "id": "week",
+                "name": "Wochenmiete",
+                "days": "7 Tage",
+                "duration_h": "168",
+                "currency": "€",
+                "price": "1600",
+                "included_km": "1000",
+                "added_km": "1,50",
+                "penalty_km": "2,00",
+                "plan": "special-elite"
+            },
+            {
+                "id": "weekend",
+                "name": "Wochenende",
+                "days": "Freitag - Sonntag",
+                "duration_h": "72",
+                "currency": "€",
+                "price": "650",
+                "included_km": "500",
+                "added_km": "1,50",
+                "penalty_km": "2,00",
+                "plan": "elite"
+            }
+        ]
+    },
+    {
+        "id": "Audi RS3 Sportback",
+        "name": "RS3 Sportback",
+        "thumbnail": "/images/audi_rs3_sportback_front_left_corner_thumbnail.jpg",
+        "images": [
+            "/images/audi_rs3_sportback_front.png",
+            "/images/audi_rs3_sportback_front_left_corner.jpg",
+            "/images/audi_rs3_sportback_interieur.png",
+            "/images/audi_rs3_sportback_stearing_wheel.png",
+            "/images/audi_rs3_sportback_rear.jpg",
+            "/images/audi_rs3_sportback_passenger_seat.png",
+            "/images/audi_rs3_sportback_rim.jpg"
+        ],
+
+        "specs": {
+            "power_hp": "400 PS",
+            "torque_nm": "500 NM",
+            "vmax_kmh": "280 km/h",
+            "acceleration_0_100": "3,7 s"
+        },
+
+        "prices": [
+            {
+                "id": "day-week",
+                "name": "Tagesmiete",
+                "days": "Montag - Donnerstag",
+                "duration_h": "24",
+                "currency": "€",
+                "price": "220",
+                "included_km": "250",
+                "added_km": "1,50",
+                "penalty_km": "2,00",
+                "plan": "basic"
+            },
+            {
+                "id": "day-weekend",
+                "name": "Tagesmiete",
+                "days": "Freitag - Sonntag",
+                "duration_h": "24",
+                "currency": "€",
+                "price": "260",
+                "included_km": "250",
+                "added_km": "1,50",
+                "penalty_km": "2,00",
+                "plan": "basic"
+            },
+            {
+                "id": "week",
+                "name": "Wochenmiete",
+                "days": "Freitag - Sonntag",
+                "duration_h": "168",
+                "currency": "€",
+                "price": "1250",
+                "included_km": "1300",
+                "added_km": "1,50",
+                "penalty_km": "2,00",
+                "plan": "special-elite"
+            },
+            {
+                "id": "weekend",
+                "name": "Wochenende",
+                "days": "Freitag - Sonntag",
+                "duration_h": "72",
+                "currency": "€",
+                "price": "500",
+                "included_km": "500",
+                "added_km": "1,50",
+                "penalty_km": "2,00",
+                "plan": "elite"
+            }
+        ]
+    },
+    {
+        "id": "VW Golf 8 R",
+        "name": "Golf 8 R",
+        "thumbnail": "/images/vw_golf_8_r_front_left_corner_thumbnail.jpeg",
+        "images": [
+            "/images/vw_golf_8_r_front.jpeg",
+            "/images/vw_golf_8_r_front_left_corner.jpeg",
+            "/images/vw_golf_8_r_left_tail_light.jpeg",
+            "/images/vw_golf_8_r_rear.jpeg",
+            "/images/vw_golf_8_r_passenger_seat.jpeg",
+            "/images/vw_golf_8_r_front_right_corner.jpeg"
+        ],
+
+        "specs": {
+            "power_hp": "320 PS",
+            "torque_nm": "420 NM",
+            "vmax_kmh": "280 km/h",
+            "acceleration_0_100": "4,3 s"
+        },
+
+        "prices": [
+            {
+                "id": "day-week",
+                "name": "Tagesmiete",
+                "days": "Montag - Donnerstag",
+                "duration_h": "24",
+                "currency": "€",
+                "price": "170",
+                "included_km": "200",
+                "added_km": "1,50",
+                "penalty_km": "2,00",
+                "plan": "basic"
+            },
+            {
+                "id": "day-weekend",
+                "name": "Tagesmiete",
+                "days": "Freitag - Sonntag",
+                "duration_h": "24",
+                "currency": "€",
+                "price": "180",
+                "included_km": "250",
+                "added_km": "1,50",
+                "penalty_km": "2,00",
+                "plan": "basic"
+            },
+            {
+                "id": "week",
+                "name": "Wochenmiete",
+                "days": "Freitag - Sonntag",
+                "duration_h": "168",
+                "currency": "€",
+                "price": "850",
+                "included_km": "1100",
+                "added_km": "1,50",
+                "penalty_km": "2,00",
+                "plan": "special-elite"
+            },
+            {
+                "id": "weekend",
+                "name": "Wochenende",
+                "days": "Freitag - Sonntag",
+                "duration_h": "72",
+                "currency": "€",
+                "price": "350",
+                "included_km": "500",
+                "added_km": "1,50",
+                "penalty_km": "2,00",
+                "plan": "elite"
+            }
+        ]
+    },
+    {
+        "id": "VW Golf 8 GTI CS",
+        "name": "Golf 8 GTI CS",
+        "thumbnail": "/images/vw_golf_8_gti_front_left_corner_thumbnail.jpeg",
+        "images": [
+            "/images/vw_golf_8_gti_front.jpeg",
+            "/images/vw_golf_8_gti_front_left_corner.jpeg",
+            "/images/vw_golf_8_gti_logo_side.jpeg",
+            "/images/vw_golf_8_gti_driver_seat.jpeg",
+            "/images/vw_golf_8_gti_stearing_wheel.jpeg",
+            "/images/vw_golf_8_gti_rear_left_corner.jpeg",
+            "/images/vw_golf_8_gti_logo_rear.jpeg",
+            "/images/vw_golf_8_gti_rear_left_tail_light.jpeg",
+            "/images/vw_golf_8_gti_rear.jpeg",
+            "/images/vw_golf_8_gti_front_right_corner.jpeg"
+        ],
+
+        "specs": {
+            "power_hp": "300 PS",
+            "torque_nm": "400 NM",
+            "vmax_kmh": "250 km/h",
+            "acceleration_0_100": "5,5 s"
+        },
+
+        "prices": [
+            {
+                "id": "day-week",
+                "name": "Tagesmiete",
+                "days": "Montag - Donnerstag",
+                "duration_h": "24",
+                "currency": "€",
+                "price": "160",
+                "included_km": "200",
+                "added_km": "1,50",
+                "penalty_km": "2,00",
+                "plan": "basic"
+
+            },
+            {
+                "id": "day-weekend",
+                "name": "Tagesmiete",
+                "days": "Freitag - Sonntag",
+                "duration_h": "24",
+                "currency": "€",
+                "price": "170",
+                "included_km": "250",
+                "added_km": "1,50",
+                "penalty_km": "2,00",
+                "plan": "basic"
+            },
+            
+            {
+                "id": "week",
+                "name": "Wochenmiete",
+                "days": "Freitag - Sonntag",
+                "duration_h": "168",
+                "currency": "€",
+                "price": "650",
+                "included_km": "1000",
+                "added_km": "1,50",
+                "penalty_km": "2,00",
+                "plan": "special-elite"
+            },
+            {
+                "id": "weekend",
+                "name": "Wochenende",
+                "days": "Freitag - Sonntag",
+                "duration_h": "72",
+                "currency": "€",
+                "price": "300",
+                "included_km": "500",
+                "added_km": "1,50",
+                "penalty_km": "2,00",
+                "plan": "elite"
+            }
+        ]
+    }
+];
